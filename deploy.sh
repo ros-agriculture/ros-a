052b12039263774b9d08ebc5f3e3a26086eedbe9
Behzad Samadi <behzad.samadi@gmail.com>
2019-03-01 23:03:12 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ rm -rf public
 git clone --depth 1 https://$GITHUB_USER:$GITHUB_TOKEN@$DEPLOY_REPO --branch $DEPLOY_BRANCH $DEPLOY_DIR
 hugo --config config.toml
 cd $DEPLOY_DIR
+ls
 git config user.email "$GITHUB_EMAIL"
 git config --global user.name "$GITHUB_USER"
 git add -A
